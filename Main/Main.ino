e58f5dd06434ce6c7d31cdf9a6e26c13c17378c7
billtsol <billtsol@protonmail.com>
2024-03-23 09:59:59 +0200

    second version of code

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,8 +1,8 @@
 #include <QTRSensors.h> //Make sure to install the library
 
 /*************************************************************************
-* QTR GPIO pins declaration
-*************************************************************************/
+ * QTR GPIO pins declaration
+ *************************************************************************/
 
 QTRSensors qtr;
 const uint8_t SensorCount = 11;
@@ -12,32 +12,31 @@ const int oddPin = 2;
 const int evenPin = 12;
 
 /*************************************************************************
-* DRV8835 GPIO pins declaration
-*************************************************************************/
+ * DRV8835 GPIO pins declaration
+ *************************************************************************/
 int mode = 13;
-int aphase = 10;
-int aenbl = 7;
+int aphase = 7; // old 10
+int aenbl = 10; // old 7
 int bphase = 4;
 int benbl = 9;
 
-int max_speed = 150;
-int min_speed = -150;
+int max_speed = 250;
+int min_speed = -250;
 
 /*************************************************************************
-* Buttons pins declaration
-*************************************************************************/
+ * Buttons pins declaration
+ *************************************************************************/
 int buttoncalibrate = 13;
 
-
 /*************************************************************************
-* PID controler variables
-*************************************************************************/
-float Kp = 0.04; //related to the proportional control term; 
-                   //change the value by trial-and-error (ex: 0.07).
-float Ki = 0.0002;  //related to the integral control term; 
-                   //change the value by trial-and-error (ex: 0.0008).
-float Kd = 0.008;  //related to the derivative control term; 
-                   //change the value by trial-and-error (ex: 0.6).
+ * PID controler variables
+ *************************************************************************/
+float Kp = 0.05; // related to the proportional control term;
+                 // change the value by trial-and-error (ex: 0.07).
+float Ki = 0.00001; // related to the integral control term;
+                    // change the value by trial-and-error (ex: 0.0008).
+float Kd = 0.5; // related to the derivative control term;
+                // change the value by trial-and-error (ex: 0.6).
 
 int P = 0;
 int I = 0;
@@ -45,13 +44,12 @@ int D = 0;
 
 int lastError = 0;
 
-int base_speed = 100;
+int base_speed = 200;
 
-void setup() {
-  Serial.begin(9600);
-  
+void setup()
+{
   qtr.setTypeRC();
-  qtr.setSensorPins((const uint8_t[]){ A0, A1, A2, A3, A4, A5, 3, 5, 6, 8, 11  }, SensorCount);
+  qtr.setSensorPins((const uint8_t[]){A5, 3, A4, 5, A3, 8, A2, 6, A1, 11, A0}, SensorCount);
   qtr.setEmitterPins(oddPin, evenPin);
 
   pinMode(mode, OUTPUT);
@@ -60,107 +58,118 @@ void setup() {
   pinMode(bphase, OUTPUT);
   pinMode(benbl, OUTPUT);
 
-  digitalWrite(mode, HIGH); //one of the two control interfaces 
+  digitalWrite(mode, HIGH); // one of the two control interfaces
 
   delay(2000);
 
   calibration(); // Calibrate the robot for 10 seconds
-
 }
 
-void loop() {
+void loop()
+{
+  // forward_movement(100, 100);
+
   PID_control();
 }
 
-void PID_control(){
-  uint16_t position = qtr.readLineBlack(sensorValues); 
+void PID_control()
+{
+  uint16_t position = qtr.readLineBlack(sensorValues);
 
   int error = 5000 - position;
- 
+
   P = error;
   I = I + error;
   D = error - lastError;
-  
+
   lastError = error;
 
-  float motor_speed = P*Kp + I*Ki + D*Kd;
-  int motor_speed_A = base_speed + motor_speed; 
+  float motor_speed = P * Kp + I * Ki + D * Kd;
+  int motor_speed_A = base_speed + motor_speed;
   int motor_speed_B = base_speed - motor_speed;
 
   // Set max speed
-  if (motor_speed_A > max_speed) { motor_speed_A = max_speed; }
-  if (motor_speed_B > max_speed) { motor_speed_B = max_speed; }
-  
+  if (motor_speed_A > max_speed)
+  {
+    motor_speed_A = max_speed;
+  }
+  if (motor_speed_B > max_speed)
+  {
+    motor_speed_B = max_speed;
+  }
+
   // Set min speed
-  if (motor_speed_A < min_speed) { motor_speed_A = min_speed; }
-  if (motor_speed_B < min_speed) { motor_speed_B = min_speed; }
+  if (motor_speed_A < min_speed)
+  {
+    motor_speed_A = min_speed;
+  }
+  if (motor_speed_B < min_speed)
+  {
+    motor_speed_B = min_speed;
+  }
 
-  Serial.print("\n");
-  Serial.print("Line: ");
-  Serial.println(position);
-  Serial.print("\n");
-  
-  Serial.print("P: ");
-  Serial.print(P);
-  Serial.print('\t');
-  Serial.print("I: ");
-  Serial.print(I);
-  Serial.print('\t');
-  Serial.print("D: ");
-  Serial.print(D);
-  Serial.print('\n');
+  // Serial.print("\n");
+  // Serial.print("Line: ");
+  // Serial.println(position);
+  // Serial.print("\n");
 
-  Serial.print("Kp: ");
-  Serial.print(Kp);
-  Serial.print('\t');
-  Serial.print("Ki: ");
-  Serial.print(Ki);
-  Serial.print('\t');
-  Serial.print("Kd: ");
-  Serial.print(Kd);
-  Serial.print('\t');
+  // Serial.print("P: ");
+  // Serial.print(P);
+  // Serial.print('\t');
+  // Serial.print("I: ");
+  // Serial.print(I);
+  // Serial.print('\t');
+  // Serial.print("D: ");
+  // Serial.print(D);
+  // Serial.print('\n');
 
-  Serial.print("sum: ");
-  Serial.print(motor_speed);
-  Serial.print('\n');
-  Serial.print('\n');
+  // Serial.print("Kp: ");
+  // Serial.print(Kp);
+  // Serial.print('\t');
+  // Serial.print("Ki: ");
+  // Serial.print(Ki);
+  // Serial.print('\t');
+  // Serial.print("Kd: ");
+  // Serial.print(Kd);
+  // Serial.print('\t');
 
-  Serial.print("Speed A: ");
-  Serial.print(motor_speed_A);
-
-  Serial.print('\n');
-  Serial.print("Speed B: ");
-  Serial.print(motor_speed_B);
+  // Serial.print("sum: ");
+  // Serial.print(motor_speed);
+  // Serial.print('\n');
+  // Serial.print('\n');
 
   forward_movement(motor_speed_A, motor_speed_B);
-
 }
 
-
-void forward_movement(int speedA, int speedB) {
-  if (speedA < 0) {
+void forward_movement(int speedA, int speedB)
+{
+  if (speedA < 0)
+  {
     speedA = 0 - speedA;
     digitalWrite(aphase, LOW);
   }
-  else {
+  else
+  {
     digitalWrite(aphase, HIGH);
-  } 
-  
-  if (speedB < 0) {
+  }
+
+  if (speedB < 0)
+  {
     speedB = 0 - speedB;
     digitalWrite(bphase, HIGH);
   }
-  else {
+  else
+  {
     digitalWrite(bphase, LOW);
   }
 
   analogWrite(aenbl, speedA);
   analogWrite(benbl, speedB);
-
 }
 
-void calibration() {
-  
+void calibration()
+{
+
   // digitalWrite(buttoncalibrate, HIGH);
   for (uint16_t i = 0; i < 400; i++)
   {
