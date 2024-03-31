526568ceae624450f426742285eac64451186c00
billtsol <billtsol@protonmail.com>
2024-04-01 00:38:30 +0300

    Version 2 in adv follow the line

--- adv_line/adv_line.ino
+++ adv_line/adv_line.ino
@@ -1,3 +1,5 @@
+// Length = 5m
+
 /*************************************************************************
  * QRT-X GPIO pins declaration
  *************************************************************************/
@@ -23,27 +25,26 @@ int aenbl = 5;
 int bphase = 6;
 int benbl = 9;
 
-int max_speed = 100;
-int min_speed = -50;
+int base_speed = 75;
 
+int max_speed = 120;
+int min_speed = -120;
 
 /*************************************************************************
 * PID controler variables
 *************************************************************************/
-float Kp = 0.05;  //related to the proportional control term;
-                   //change the value by trial-and-error (ex: 0.07).
-float Ki = 0.00001; //related to the integral control term;
-                   //change the value by trial-and-error (ex: 0.0008).
-float Kd = 0.3;  //related to the derivative control term;
-                   //change the value by trial-and-error (ex: 0.6).
+float Kp = 0.04;  //related to the proportional control term;
+                   //change the value by trial-and-error (ex: 0.07). 0.03
+float Ki = 0.0009; //related to the integral control term;
+                   //change the value by trial-and-error (ex: 0.0008). 0.00001
+float Kd = 0.4;  //related to the derivative control term;
+                   //change the value by trial-and-error (ex: 0.6). 0.3
 
 int P = 0;
 int I = 0;
 int D = 0;
 
 int lastError = 0;
-
-int base_speed = 50;
 
 /*************************************************************************
 * Buttons pins declaration
@@ -54,10 +55,10 @@ int startButton = A7;
 void setup() {
   Serial.begin(9600);
 
-  QTRX Set up
+  // QTRX Set up
   qtr.setTypeRC();
   qtr.setSensorPins((const uint8_t[]){4, 7, 8, 10, 11, 12, A0, A1, A2, A3, A4}, SensorCount);
-  qtr.setEmitterPins(oddPin,evenPin);
+  qtr.setEmitterPin(oddPin);
 
   // Motors Set up
   pinMode(aphase, OUTPUT);
@@ -65,14 +66,18 @@ void setup() {
   pinMode(bphase, OUTPUT);
   pinMode(benbl, OUTPUT);
 
+  pinMode(13,OUTPUT);
+
   pinMode(buttoncalibrate, INPUT);
   pinMode(startButton, INPUT);
 
-  while( analogRead(buttoncalibrate) == 0); // Start Calibration
+  digitalWrite(13, LOW);
+
+  while( analogRead(buttoncalibrate) == 0); // Start Calibration A6
 
   calibration(); // Calibrate the robot for 10 seconds
 
-  while( analogRead(startButton) == 0); // Start Robot
+  while( analogRead(startButton) == 0); // Start Calibration A6
 
 }
 
@@ -92,8 +97,8 @@ void PID_control(){
   lastError = error;
 
   float motor_speed = P*Kp + I*Ki + D*Kd;
-  int motor_speed_A = base_speed + motor_speed;
-  int motor_speed_B = base_speed - motor_speed;
+  int motor_speed_A = base_speed - motor_speed;
+  int motor_speed_B = base_speed + motor_speed;
 
   // Set max speed
   if (motor_speed_A > max_speed) { motor_speed_A = max_speed; }
@@ -103,31 +108,34 @@ void PID_control(){
   if (motor_speed_A < min_speed) { motor_speed_A = min_speed; }
   if (motor_speed_B < min_speed) { motor_speed_B = min_speed; }
 
-  forward_movement(motor_speed_A, motor_speed_B);
-}
+  // pid_test( position, motor_speed);
 
-void calibration() {
-  for (uint16_t i = 0; i < 400; i++) { qtr.calibrate(); }
+  forward_movement(motor_speed_A, motor_speed_B);
+
+  // Serial.print('\n');
+  // Serial.print("A: ");
+  // Serial.print(motor_speed_A);
+  // Serial.print('\n');
+  // Serial.print("B: ");
+  // Serial.print(motor_speed_B);
+
+  // Serial.print('\n');
 }
 
 void forward_movement(int speedA, int speedB) {
-  if (speedA < 0)
-  {
+  if (speedA < 0) {
     speedA = 0 - speedA;
     digitalWrite(aphase, LOW);
   }
-  else
-  {
+  else {
     digitalWrite(aphase, HIGH);
   }
 
-  if (speedB < 0)
-  {
+  if (speedB < 0) {
     speedB = 0 - speedB;
     digitalWrite(bphase, HIGH);
   }
-  else
-  {
+  else {
     digitalWrite(bphase, LOW);
   }
 
@@ -135,12 +143,20 @@ void forward_movement(int speedA, int speedB) {
   analogWrite(benbl, speedB);
 }
 
+void calibration() {
+  digitalWrite(13,HIGH);
+  for (uint16_t i = 0; i < 400; i++) { qtr.calibrate(); }
+  digitalWrite(13,LOW);
+}
+
+
 //  Testing functions
 void qtrx_test(){
   uint16_t position = qtr.readLineBlack(sensorValues);
 
   for (uint8_t i = 0; i < SensorCount; i++)
   {
+
     Serial.print(sensorValues[i]);
     Serial.print('\t');
   }
@@ -148,7 +164,6 @@ void qtrx_test(){
 
   delay(250);
 }
-
 
 void pid_test(int position,int motor_speed){
 
